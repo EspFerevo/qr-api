dc3ce7aa41d3592366698da0ad9cbb0a943113a2
unknown <olekchekrenov@gmail.com>
2025-02-27 17:17:18 +0200

    Update Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import '../styles/App.css'
+
+function Footer() { 
+    <footer className="footer"> 
+        <p>
+        © 2025 QR Generator. Все права защищены.
+        </p>
+    </footer>
+
+}
+
+export default Footer;
