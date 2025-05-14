70ef6ef51fe736952f700563ef5e4387ba9735f0
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-05-14 12:02:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const QRCode = require('qrcode');
-const cors = require('cors')
-
+const express = require("express");
+const QRCode = require("qrcode");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get('/generate', async (req, res) => {
+app.get("/generate", async (req, res) => {
   const url = req.query.url;
 
   if (!url) {
-    return res.status(400).send('URL не указан.');
+    return res.status(400).send("URL не указан.");
   }
 
   try {
     const qr = await QRCode.toDataURL(url);
     res.send(`<img src="${qr}" />`);
   } catch (err) {
-    res.status(500).send('Ошибка при создании QR-кода.');
+    res.status(500).send("Ошибка при создании QR-кода.");
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен по адресу http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
